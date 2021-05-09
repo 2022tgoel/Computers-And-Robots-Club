89d48359f6ca0e7a39e9f6f5645e2eab917d32b2
Tarushii Goel <tishi.tng@gmail.com>
2021-05-08 20:39:13 -0400

    add algorithm code

--- Maze Code Lab/mazeAlgorithm.py
+++ Maze Code Lab/mazeAlgorithm.py
@@ -2,22 +2,40 @@ from robot import Robot
 
 robot = Robot()
 
-#CODE FOR THE ALGORITHM GOES HERE
-
-
-'''
-just code I used to test if it works
-remove later
 robot.printMaze()
-robot.turnLeft()
-print(robot.canMoveForward())
-robot.turnRight()
-print(robot.canMoveForward())
-robot.moveForward()
-robot.printMaze()
-while(robot.canMoveForward()):
-	robot.moveForward()
 
-robot.printMaze()
-print(robot.canMoveForward())
-'''
+print("STARTING ALGORITHM")
+
+def isLeftOpen():
+    robot.turnLeft()
+    leftOpen = robot.canMoveForward()
+    robot.turnRight()
+    return leftOpen
+def isFrontOpen():
+    return robot.canMoveForward()
+def isRightOpen():
+    robot.turnRight()
+    rightOpen = robot.canMoveForward()
+    robot.turnLeft()
+    return rightOpen
+def turnAround():
+    robot.turnLeft()
+    robot.turnLeft()
+
+def navigateMaze():
+    while not robot.isDone():
+        if robot.willReachEnd():
+            robot.moveForward()
+        elif isLeftOpen():
+            robot.turnLeft()
+        elif isFrontOpen():
+            robot.moveForward()
+        elif isRightOpen():
+            robot.turnRight()
+        else:
+            turnAround()
+        robot.printMaze()
+        v = raw_input("press enter to move to the next step")
+
+navigateMaze()
+
--- Maze Code Lab/robot.py
+++ Maze Code Lab/robot.py
@@ -10,7 +10,7 @@ class Robot():
 	def read_input(self):
 		f = open(MAZE_FILE, "r")
 		self.h, self.w = [int(x) for x in f.readline().split(" ")]
-		self.d = [-1, -1*self.h, 1, self.h]
+		self.d = [-1, -1*self.w, 1, self.w]
 		maze = ""
 		for i in range(self.h):
 			maze+=f.readline().strip()
@@ -35,18 +35,27 @@ class Robot():
 			return False
 		if (self.o == 3 and self.loc//self.w==self.h-1):
 			return False
-		if (self.maze[self.loc + self.d[self.o]]!=B):
+		if (self.maze[self.loc + self.d[self.o]]!=B and self.maze[self.loc + self.d[self.o]]!=S):
 			return True 
-		print("here")
 		return False
 	def moveForward(self):
 		if not self.canMoveForward():
 			return ## might want to throw an error?
 		self.loc = self.loc + self.d[self.o]
 	def turnLeft(self):
-		self.o = (self.o+1)%4
-	def turnRight(self):
 		self.o = (self.o+3)%4
+	def turnRight(self):
+		self.o = (self.o+1)%4
+	def willReachEnd(self):
+        if (self.o == 0 and self.loc%self.w==0):
+			return False
+		if (self.o == 1 and self.loc//self.w==0):
+			return False
+		if (self.o == 2 and self.loc%self.w==self.w-1):
+			return False
+		if (self.o == 3 and self.loc//self.w==self.h-1):
+			return False
+        return self.loc + self.d[self.o] == self.end
 	def isDone(self):
 		return self.loc == self.end
 	def printMaze(self):
